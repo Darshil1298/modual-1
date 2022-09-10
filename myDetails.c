201c85acf69380bcc26e59da06be9401e6910018
Darshil <dmp1298@gmail.com>
2022-09-10 13:15:22 +0530

    my details

---
+++ myDetails.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+ int main()  
+  {
+     printf("Name : Darshil Patel\n"); 
+     printf("DOB  : 1st Feb 1998\n"); 
+     printf("Age : 24\n"); 
+     printf("Address : Surat \n"); 
+     return(0); 
+  }
